f3fdb0c08bed0719619f8539345bba36ab1fd565
Silvio Molina <kindermolina98@gmail.com>
2020-11-18 16:26:54 -0300

    first commit

---
+++ newProject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from newProject import views 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('initial/', views.hello),
+]
---
+++ newProject/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+import pdb
+
+def hello(request):
+
+	return HttpResponse('Hola')
