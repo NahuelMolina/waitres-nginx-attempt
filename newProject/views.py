1c2087b484016e97df5932bb24eab0a45fd6fcd9
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-11-30 17:14:33 -0300

    Update views.py

--- newProject/views.py
+++ newProject/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 import pdb
 
 def hello(request):
+    return HttpResponse('Hola, world')
 
-	return HttpResponse('Hola, world')
+def houses_views(request):
+    return HttpResponse('views of houses')
