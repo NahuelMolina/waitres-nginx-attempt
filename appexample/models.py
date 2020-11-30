d04ed09a8497c3e964415ebf686525477941cbe4
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-11-30 17:09:56 -0300

    Update models.py

--- appexample/models.py
+++ appexample/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.http import HttpResponseRedirect
 
-# Create your models here.
+class houses(models.Model):
+    size_meters = models.IntegerField()
+    address = models.TextField(50)
+    vendor_name = models.CharField(25)
+    
+    def __str__(self):
+        return self.vendor_name
+    
+    def get_absolute_url():
+        return HttpResponseRedirect(reverse('house_url'))
